5dd447a64020202c705f6bb86df934f2bc27194e
lukasz-starosta <lstarosta98@gmail.com>
2021-11-14 20:48:53 +0100

    Preparing data for classification

--- task-2/prepare/get_prepared_data.py
+++ task-2/prepare/get_prepared_data.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from os.path import exists
+
+from prepare.factorize import factorize
 from prepare.prepare_data import prepare_data
 
 # Relative to python invocation
@@ -8,10 +10,24 @@ DATA_PATH = './data/task-2.csv'
 file_exists = exists(DATA_PATH)
 
 
-def get_prepared_data():
+def get_cleaned_data():
     if (not file_exists):
         dataframe = prepare_data()
         pd.DataFrame.to_csv(dataframe, DATA_PATH)
         return dataframe
 
     return pd.read_csv(DATA_PATH)
+
+
+def get_prepared_data():
+    data = get_cleaned_data()
+
+    # labels
+    labels = data[data.columns[-1]].unique()
+
+    # transform all records' columns values into numbers
+    columns = data.columns[1:]
+    for col in columns:
+        data[col] = factorize(data[col])
+
+    return data, labels
--- task-2/prepare/prepare_data.py
+++ task-2/prepare/prepare_data.py
@@ -1,5 +1,29 @@
+import numpy as np
 import pandas as pd
+from pandas.core.algorithms import factorize
+
+MAIN_DATAFILE_PATH = '../data/NYPD_Complaint_Data_Historic.csv'
 
 
 def prepare_data():
-    return pd.DataFrame.from_dict({})
+    initial_data = pd.read_csv(
+        MAIN_DATAFILE_PATH, na_values=np.nan)
+
+    # get only selected columns from the dataset
+    data = initial_data[['LAW_CAT_CD', 'BORO_NM', 'PREM_TYP_DESC',
+                         'SUSP_AGE_GROUP', 'SUSP_RACE', 'VIC_AGE_GROUP', 'VIC_RACE', 'VIC_SEX', 'SUSP_SEX']]
+
+    # delete records which do not have suspect's sex defined - they provide no value
+    data = data[data['SUSP_SEX'].isin([
+        'M', 'F'])]
+
+    # replace UNKNOWN for np.nan
+    data.replace('UNKNOWN', np.nan, inplace=True)
+
+    # delete records with unknown/no value
+    data.dropna(subset=['BORO_NM', 'VIC_RACE', 'PREM_TYP_DESC', 'VIC_AGE_GROUP',
+                'VIC_SEX', 'SUSP_AGE_GROUP', 'SUSP_RACE'], inplace=True)
+
+    # at least 1 mil rows should be left
+
+    return data
