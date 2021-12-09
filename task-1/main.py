12f20b77f49daac7180e619bb0f53b31fa5243d4
jedrek <jedrekszor@wp.pl>
2021-12-09 10:21:57 +0100

    task1 checkpoint 3

--- task-1/main.py
+++ task-1/main.py
@@ -1,51 +1,75 @@
 import datetime
+import os
 from prepare.get_prepared_data import get_prepared_data
-import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+from sklearn.metrics import mean_squared_error
+
+months = 12
+
+
+# degree = 8
+
+
+def perform_regression(ds, name):
+    path = "results/" + str(degree)
+
+    ds.index = ds.index.rename("year", level=0)
+    ds.index = ds.index.rename("month", level=1)
+    ds = ds.rename("amount", level=0)
+    ds = ds.reset_index()
+
+    ds['date'] = ds.apply(
+        lambda x: datetime.datetime.timestamp(datetime.datetime(x['year'].astype(int), x['month'].astype(int), 1)),
+        axis=1)
+    final = ds[['date', 'amount']]
+
+    # queens_train_x, queens_test_x, queens_train_y, queens_test_y = train_test_split(final['date'], final['amount'])
+
+    ds_train_x = final['date'][:-months]
+    ds_test_x = final['date'][-months:]
+    ds_train_y = final['amount'][:-months]
+    ds_test_y = final['amount'][-months:]
+
+    # scaler = StandardScaler()
+    polyreg_scaled = make_pipeline(PolynomialFeatures(degree), LinearRegression())
+    polyreg_scaled.fit(np.array(ds_train_x).reshape(-1, 1), ds_train_y)
+
+    prediction = polyreg_scaled.predict(np.array(ds_test_x).reshape(-1, 1))
+    x, y = zip(*sorted(zip(ds_test_x, prediction), key=lambda q: q[0]))
+
+    mse = np.sqrt(mean_squared_error(ds_test_y, prediction))
+
+    plt.plot(ds_train_x, ds_train_y, label="Data", color='b', alpha=.5)
+    plt.plot(x, y, label="Prediction", color='r', alpha=.7)
+    plt.legend()
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    plt.savefig(path + "/" + name + "_" + str(mse) + ".jpg")
+    plt.clf()
+    return mse
+
 
 queens, bronx, brooklyn, manhattan, staten = get_prepared_data()
 
-queens.index = queens.index.rename("year", level=0)
-queens.index = queens.index.rename("month", level=1)
-queens = queens.rename("amount", level=0)
-queens = queens.reset_index()
+queens_mse, bronx_mse, brooklyn_mse, manhattan_mse, staten_mse = [], [], [], [], []
+max_degree = 20
+for degree in range(1, max_degree + 1):
+    print("Degree ", degree, " of ", max_degree)
+    queens_mse.append(perform_regression(queens, "queens"))
+    bronx_mse.append(perform_regression(bronx, "bronx"))
+    brooklyn_mse.append(perform_regression(brooklyn, "brooklyn"))
+    manhattan_mse.append(perform_regression(manhattan, "manhattan"))
+    staten_mse.append(perform_regression(staten, "staten"))
 
-queens['date'] = queens.apply(lambda x:datetime.datetime.timestamp(datetime.datetime(x['year'].astype(int), x['month'].astype(int), 1)), axis=1)
-final = queens[['date', 'amount']]
-queens_train_x, queens_test_x, queens_train_y, queens_test_y = train_test_split(final['date'], final['amount'])
-
-# pd.set_option("max_rows", None)
-# print(queens)
-#
-# x=queens.index, y=queens.values
-
-# print(final)
-# print(queens_train_x.shape)
-lr = LinearRegression()
-lr.fit(np.array(queens_train_x).reshape(-1, 1), queens_train_y)
-
-
-prediction = lr.predict(np.array(queens_test_x).reshape(-1, 1))
-
-
-# final.plot(kind="bar", x='date', y='amount')
-plt.plot(final['date'], final['amount'], label="Data", color='b', alpha=.5)
-plt.plot(queens_test_x, prediction, label="Linear regression", color='r', alpha=.7)
-plt.show()
-#
-# bronx.plot(kind="bar")
-# plt.show()
-#
-# brooklyn.plot(kind="bar")
-# plt.show()
-#
-# manhattan.plot(kind="bar")
-# plt.show()
-#
-# staten.plot(kind="bar")
-# plt.show()
-
-
+print()
+print("Queens: ", min(range(len(queens_mse)), key=queens_mse.__getitem__) + 1)
+print("Bronx: ", min(range(len(bronx_mse)), key=bronx_mse.__getitem__) + 1)
+print("Brooklyn: ", min(range(len(brooklyn_mse)), key=brooklyn_mse.__getitem__) + 1)
+print("Manhattan: ", min(range(len(manhattan_mse)), key=manhattan_mse.__getitem__) + 1)
+print("Staten Island: ", min(range(len(staten_mse)), key=staten_mse.__getitem__) + 1)
--- task-1/prepare/get_prepared_data.py
+++ task-1/prepare/get_prepared_data.py
@@ -10,12 +10,18 @@ file_exists = exists(DATA_PATH)
 
 
 def get_cleaned_data():
-    if (not file_exists):
+    if not file_exists:
         dataframe = prepare_data()
         pd.DataFrame.to_csv(dataframe, DATA_PATH)
         return dataframe
 
     return pd.read_csv(DATA_PATH)
+
+
+def group_by_dates(ds):
+    date = pd.to_datetime(ds['CMPLNT_FR_DT'], format='%m/%d/%Y', errors='coerce')
+    ds = ds[date.dt.year >= 2006]
+    return ds.groupby(by=[date.dt.year, date.dt.month]).size()
 
 
 def get_prepared_data():
@@ -32,24 +38,5 @@ def get_prepared_data():
     staten = data[data['BORO_NM'].isin([
         'STATEN ISLAND'])]
 
-    date = pd.to_datetime(queens['CMPLNT_FR_DT'], format='%m/%d/%Y', errors='coerce')
-    queens = queens[date.dt.year >= 2006]
-    queens_grouped = queens.groupby(by=[date.dt.year, date.dt.month])
-
-    date = pd.to_datetime(bronx['CMPLNT_FR_DT'], format='%m/%d/%Y', errors='coerce')
-    bronx = bronx[date.dt.year >= 2006]
-    bronx_grouped = bronx.groupby(by=[date.dt.year, date.dt.month])
-
-    date = pd.to_datetime(brooklyn['CMPLNT_FR_DT'], format='%m/%d/%Y', errors='coerce')
-    brooklyn = brooklyn[date.dt.year >= 2006]
-    brooklyn_grouped = brooklyn.groupby(by=[date.dt.year, date.dt.month])
-
-    date = pd.to_datetime(manhattan['CMPLNT_FR_DT'], format='%m/%d/%Y', errors='coerce')
-    manhattan = manhattan[date.dt.year >= 2006]
-    manhattan_grouped = manhattan.groupby(by=[date.dt.year, date.dt.month])
-
-    date = pd.to_datetime(staten['CMPLNT_FR_DT'], format='%m/%d/%Y', errors='coerce')
-    staten = staten[date.dt.year >= 2006]
-    staten_grouped = staten.groupby(by=[date.dt.year, date.dt.month])
-
-    return queens_grouped.size(), bronx_grouped.size(), brooklyn_grouped.size(), manhattan_grouped.size(), staten_grouped.size()
+    return group_by_dates(queens), group_by_dates(bronx), group_by_dates(brooklyn), group_by_dates(
+        manhattan), group_by_dates(staten)
